b7ddd6416067a09c5c8d8925b95e4c7f4aa3301f
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-03-23 10:35:37 -0400

    Add basic archive script

---
+++ http-archive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get mirror
+wget --mirror --page-requisites --html-extension --convert-links -x -e robots=off -P . $1
+
+# Remove wget index.html to keep original folders index urls
+LC_CTYPE=C LANG=C find . -name \*.html | xargs sed -i.bak "s/index\.html//g"
+
+# Publish to Amazon S3 website enabled bucket
+s3cmd mb s3://$2
+s3cmd put --acl-public --recursive $1/  s3://$2
